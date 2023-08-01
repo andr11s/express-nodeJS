a606739752d8973402a3616b71b89a33a4236961
Alvis <aherrera@munily.com>
2023-08-01 10:23:53 -0500

    feat: update router

--- src/router/app.router.js
+++ src/router/app.router.js
@@ -1,13 +1,11 @@
-const userRouter = require("../router/userRouter");
-const productsRouter = require("../router/productsRouter")
+const userRouter = require('../router/userRouter')
+const productsRouter = require('../router/productsRouter')
 
-function rotuerApi(app) {
+function rotuerApi (app) {
   // const router = express.Router()
   // app.use("/api/v1", router);
-
-
   app.use('/users', userRouter)
   app.use('/products', productsRouter)
 }
 
-module.exports = rotuerApi;
+module.exports = rotuerApi
